53ccb1dbfa9880f4fe275e9d700960f935f0b44a
holovanist <jonsonlogan37@gmail.com>
2025-12-02 13:37:22 -0800

    rpc mostly working

--- Multiplayer Game/Assets/Scripts/Scripts/Movement/PlayerMovement1.cs
+++ Multiplayer Game/Assets/Scripts/Scripts/Movement/PlayerMovement1.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : NetworkBehaviour
 {
     [Header("Movement")]
     private float moveSpeed;
-    public float walkSpeed,sprintSpeed,maxYSpeed,GroundDrag;
+    public float walkSpeed,sprintSpeed,maxYSpeed,GroundDrag,AirDrag;
+    public bool AddAirDrag;
 
     [Header("Jumping")]
     public float jumpForce,jumpCooldown,airMultiplier;
@@ -29,8 +30,7 @@ public class PlayerMovement : NetworkBehaviour
     [Header("References")]
     public Transform orientation;
     public TextMeshProUGUI Speed;
-    float horizontalInput;
-    float verticalInput;
+    Vector2 MoveInput;
     Vector3 moveDir;
     public Rigidbody rb;
     //private StarterAssetsInputs it;
@@ -52,6 +52,8 @@ public class PlayerMovement : NetworkBehaviour
     InputAction Crouch;
     InputAction JumpInput;
     InputAction Sprint;
+    bool crouch;
+    bool sprint;
     private void Awake()
     {
         Move = InputSystem.actions.FindAction("Move");
@@ -72,56 +74,58 @@ public class PlayerMovement : NetworkBehaviour
     private void Update()
     {
         if (!IsOwner) return;
-
-
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
-        MyInputRpc();
-        SpeedControl();
+        SetVariablesRpc();
+        MyInput();
+        SpeedControlRpc();
         StateHandler();
+
+    }
+    [Rpc(SendTo.Server)]
+    private void SetVariablesRpc()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
         if (grounded)
             rb.linearDamping = GroundDrag;
+        else if (AddAirDrag && !grounded)
+            rb.linearDamping = AirDrag;
         else
             rb.linearDamping = 0f;
-        if(Speed != null)
-        Speed.SetText("Speed: " + string.Format("{0:0.00}", rb.linearVelocity.magnitude));
+        if (Speed != null)
+            Speed.SetText("Speed: " + string.Format("{0:0.00}", rb.linearVelocity.magnitude));
     }
     private void FixedUpdate()
     {
         if (!IsOwner) return;
-        MovePlayerRpc();
+        if (IsServer && IsLocalPlayer)
+            MovePlayer(MoveInput);
+        else if (IsLocalPlayer)
+        MovePlayerRpc(MoveInput, orientation.forward, orientation.right, moveSpeed);
     }
-    [Rpc(SendTo.Server)]
-    private void MyInputRpc()
+    private void MyInput()
     {
-        horizontalInput = Move.ReadValue<Vector2>().x;
-        verticalInput = Move.ReadValue<Vector2>().y;
+        MoveInput = Move.ReadValue<Vector2>();
+        crouch = Crouch.IsPressed();
+        sprint = Sprint.IsPressed();
         if (JumpInput.WasPressedThisFrame() && readyToJump && grounded)
         {
             readyToJump = false;
             JumpRpc();
             Invoke(nameof(ResetJump), jumpCooldown);
         }
-        if (Crouch.WasPressedThisFrame() && !crouching && grounded)
+        if (crouch  && grounded)
         {
-            crouching = true;
-            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
-            transform.position = new Vector3(transform.position.x, transform.position.y - crouchYScale, transform.position.z);
-        }
-        else if(Crouch.WasPressedThisFrame() && grounded)
-        {
-            crouching = false;
-            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
-            transform.position = new Vector3(transform.position.x, transform.position.y + crouchYScale, transform.position.z);
+            CrouchingRpc();
         }
     }
     private void StateHandler()
     {
-        if (Crouch.WasPressedThisFrame())
+        if (crouch)
         {
             state = MovementState.crouching;
             moveSpeed = crouchSpeed;
         }
-        else if (grounded && Sprint.IsPressed())
+        else if (grounded && sprint)
         {
             state = MovementState.sprinting;
             moveSpeed = sprintSpeed;
@@ -140,15 +144,42 @@ public class PlayerMovement : NetworkBehaviour
         }
     }
     [Rpc(SendTo.Server)]
-    private void MovePlayerRpc()
+    private void CrouchingRpc()
     {
-        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (!crouching)
+        {
+            crouching = true;
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - crouchYScale, transform.position.z);
+        }
+        else
+        {
+            crouching = false;
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y + crouchYScale, transform.position.z);
+        }
+    }
+    [Rpc(SendTo.Server)]
+    private void MovePlayerRpc(Vector2 _MoveInput, Vector3 _ForwardOrientation, Vector3 _RightOrientation, float MoveSpeed)
+    {
+        MoveInput = _MoveInput;
+        moveDir = _ForwardOrientation * _MoveInput.y + _RightOrientation * _MoveInput.x;
+        rb.AddForce(10f * moveSpeed * moveDir.normalized, ForceMode.Force);
         if (grounded)
-            rb.AddForce(10f * moveSpeed * moveDir.normalized, ForceMode.Force);
+            rb.AddForce(10f * MoveSpeed * moveDir.normalized, ForceMode.Force);
         else if(!grounded)
             rb.AddForce(10f * airMultiplier * moveSpeed * moveDir.normalized, ForceMode.Force);
     }
-    private void SpeedControl()
+    private void MovePlayer(Vector2 _MoveInput)
+    {
+        moveDir = orientation.forward * _MoveInput.y + orientation.right * _MoveInput.x;
+        if (grounded)
+            rb.AddForce(10f * moveSpeed * moveDir.normalized, ForceMode.Force);
+        else if (!grounded)
+            rb.AddForce(10f * airMultiplier * moveSpeed * moveDir.normalized, ForceMode.Force);
+    }
+    [Rpc(SendTo.Server)]
+    private void SpeedControlRpc()
     {
         Vector3 flatVel = new(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         if(flatVel.magnitude > moveSpeed)
