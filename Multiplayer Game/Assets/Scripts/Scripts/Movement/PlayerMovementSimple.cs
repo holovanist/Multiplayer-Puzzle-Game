c74b106701df53d8007100bd75e86ba381956a4e
ToastyKitty48 <26.Cooper.Lyon@ksd.org>
2025-12-02 11:47:10 -0800

    adding Cinemachine package and comments to code

--- Multiplayer Game/Assets/Scripts/Scripts/Movement/PlayerMovementSimple.cs
+++ Multiplayer Game/Assets/Scripts/Scripts/Movement/PlayerMovementSimple.cs
@@ -13,10 +13,12 @@ public class PlayerMovementSimple : MonoBehaviour
     }
     private void Update()
     {
+        //Set moveDir to move action imput
         moveDirection = move.action.ReadValue<Vector2>();
     }
     void FixedUpdate()
     {
+        //Set player rb velocity to imput dir * move speed
         rb.linearVelocity = new Vector3(moveDirection.x * moveSpeed, rb.linearVelocity.y , moveDirection.y * moveSpeed);
     }
 }
