87e27fd7de9f2e9e4b3e968340a22bbe9f8e97e9
holovanist <jonsonlogan37@gmail.com>
2026-01-30 11:52:01 -0800

    tagged the player object

--- Multiplayer Game/Assets/Temp/CreatePicture.cs
+++ Multiplayer Game/Assets/Temp/CreatePicture.cs
@@ -5,4 +5,17 @@ public class CreatePicture : MonoBehaviour
     [SerializeField] GameObject PicturePrefab;
     [SerializeField] GameObject Camera;
     [SerializeField] float TimePictureIsSpawned;
+    StoredObjectData StoredData;
+    PlayerInputHandler PlayerInput;
+    private void Start()
+    {
+        StoredData= GetComponent<StoredObjectData>();
+    }
+    private void Update()
+    {
+        if(StoredData.IsHeld && PlayerInput == null)
+        {
+            PlayerInput = GetComponentInParent<PlayerInputHandler>();
+        }
+    }
 }
