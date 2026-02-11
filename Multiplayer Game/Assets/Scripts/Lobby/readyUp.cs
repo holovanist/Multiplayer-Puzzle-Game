6a42f32f3e3fe61e8d5286b1313f8525993e9533
tiredJoblin <riley.lorenzo@gmail.com>
2026-02-11 13:07:26 -0800

    got a small foundation to the ready up screen

--- Multiplayer Game/Assets/Scripts/Lobby/readyUp.cs
+++ Multiplayer Game/Assets/Scripts/Lobby/readyUp.cs
@@ -1,16 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class readyUp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject readyUpText;
+
+    public bool readyOrNot = false;
+
+    public void letsReadyUp()
     {
+        if (readyOrNot)
+        {
+            readyUpText.GetComponent<TMP_Text>().text = "NOT READY";
+            readyOrNot = false;
+        }
+        else
+        {
+            readyUpText.GetComponent<TMP_Text>().text = "READY";
+            readyOrNot = true;
+        }
+
         
     }
 }
