34fa197135993f273825b0fccef671d9eb436a72
holovanist <jonsonlogan37@gmail.com>
2026-03-09 13:32:51 -0700

    working on monitor

--- Multiplayer Game/Assets/Temp/Monitor.cs
+++ Multiplayer Game/Assets/Temp/Monitor.cs
@@ -17,8 +17,9 @@ public class Monitor : MonoBehaviour
     }
     public void MoveCamera(Camera pos)
     {
-        if(Vector3.Distance(pos.transform.position, CameraPosition.position) > 0.2)
+        if(Vector3.Distance(pos.transform.position, CameraPosition.position) > 0.2f)
         {
+            Cam.GetComponentInParent<PlayerMovement>().enabled = false;
             Vector3.Lerp(pos.transform.position, CameraPosition.position, .1f);
         }
         else
