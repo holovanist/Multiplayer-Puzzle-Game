fd5070cff778c51235e228a0fd4f2df76de748ec
ToastyKitty48 <cooper.lyon@charter.net>
2026-03-18 14:00:25 -0700

    Working on level 5

--- Multiplayer Game/Assets/Scripts/Objects/ShipLauncher.cs
+++ Multiplayer Game/Assets/Scripts/Objects/ShipLauncher.cs
@@ -7,24 +7,35 @@ public class ShipLauncher : MonoBehaviour
     [SerializeField] GameObject shipArmObject;
     [SerializeField] GameObject shipScreenMatSwapObject;
     [SerializeField] GameObject shipDoorTriggerObject;
+    [SerializeField] GameObject whaleTerrainParentObject;
     [SerializeField] Material shipScreenMatSwap1;
     [SerializeField] Material shipScreenMatSwap2;
-    [SerializeField] float ScreenWarningBlinkInterval = 0.5f;
-    [SerializeField] float ShipArmRotateSpeed = 0.2f;
-    [SerializeField] float ShipArmRotateAmount = 90;
+    [SerializeField] ShipDoorOpener shipDoorOpener;
+    [SerializeField] float screenWarningBlinkInterval = 0.5f;
+    [SerializeField] float shipArmRotateSpeed = 0.2f;
+    [SerializeField] float shipArmRotateAmount = 90;
+    [SerializeField] bool skipPlayerCountCheck = true;
     MeshRenderer screenMR;
     bool screenState = true;
+    int currentFlightStep = 1;
+    Vector3 startingPos;
     private void Start()
     {
         //gets mesh render for the main screen
         screenMR = shipScreenMatSwapObject.GetComponent<MeshRenderer>();
+        startingPos = whaleTerrainParentObject.transform.position;
     }
     //main funtion that gets called to start the whole launch sequence
     public void StartShipLaunch()
     {
-        ShipScreenMaterialSwap();
-        RotateShipArm();
-        LockDoor();
+        if (CheckIfLaunchConditionsMet())
+        {
+            ShipScreenMaterialSwap();
+            RotateShipArm();
+            LockDoor();
+            Invoke("ShipFlight", 10);
+        }
+
     }
     void ShipScreenMaterialSwap()
     {
@@ -40,22 +51,53 @@ public class ShipLauncher : MonoBehaviour
             screenMR.material = shipScreenMatSwap2;
         }
         //Triggers itself at set interval
-        Invoke("ShipScreenMaterialSwap", ScreenWarningBlinkInterval);
+        Invoke("ShipScreenMaterialSwap", screenWarningBlinkInterval);
     }
     void RotateShipArm()
     {
         //rotates ship arm and stops at set rotation amount
-        shipArmObject.transform.eulerAngles = new Vector3 (shipArmObject.transform.eulerAngles.x, shipArmObject.transform.eulerAngles.y, shipArmObject.transform.eulerAngles.z + ShipArmRotateSpeed);
-        if (shipArmObject.transform.eulerAngles.z < ShipArmRotateAmount)
+        shipArmObject.transform.eulerAngles = new Vector3 (shipArmObject.transform.eulerAngles.x, shipArmObject.transform.eulerAngles.y, shipArmObject.transform.eulerAngles.z + shipArmRotateSpeed);
+        if (shipArmObject.transform.eulerAngles.z < shipArmRotateAmount)
         {
             Invoke("RotateShipArm", 0.02f);
         } else
         {
-            shipArmObject.transform.eulerAngles = new Vector3(shipArmObject.transform.eulerAngles.x, shipArmObject.transform.eulerAngles.y, ShipArmRotateAmount);
+            shipArmObject.transform.eulerAngles = new Vector3(shipArmObject.transform.eulerAngles.x, shipArmObject.transform.eulerAngles.y, shipArmRotateAmount);
         }
     }
     void LockDoor()
     {
+        //disables door opener code
+        shipDoorOpener.enabled = false;
+    }
+    bool CheckIfLaunchConditionsMet()
+    {
+        if (skipPlayerCountCheck)
+        {
+            return true;
+        } else
+        {
+            //place the code to check if both players are in the ship here!
+            return false;
+        }
+        
+    }
+    void ShipFlight()
+    {
+        if (currentFlightStep == 1)
+        {
 
+        }
+    }
+    void ShipFlightX()
+    {
+        if (Vector3.Distance(startingPos, transform.position) >= 100)
+        {
+
+        }
+    }
+    void ShipFlightY()
+    {
+        
     }
 }
