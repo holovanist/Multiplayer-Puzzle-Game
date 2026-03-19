9df22a0fcb3db9dc5371152bf3bb6cd3582213b1
ToastyKitty48 <cooper.lyon@charter.net>
2026-03-19 13:58:21 -0700

    Working on space ship implementation

--- Multiplayer Game/Assets/Scripts/Objects/ShipLauncher.cs
+++ Multiplayer Game/Assets/Scripts/Objects/ShipLauncher.cs
@@ -1,6 +1,4 @@
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ShipLauncher : MonoBehaviour
 {
@@ -15,15 +13,18 @@ public class ShipLauncher : MonoBehaviour
     [SerializeField] float shipArmRotateSpeed = 0.2f;
     [SerializeField] float shipArmRotateAmount = 90;
     [SerializeField] bool skipPlayerCountCheck = true;
+    [SerializeField] float maxFlightSpeed = 0.5f;
     MeshRenderer screenMR;
     bool screenState = true;
     int currentFlightStep = 1;
-    Vector3 startingPos;
+    float lastSpeedUsed = 0.01f;
+    bool useMaxSpeed = false;
+    bool useMaxSpeed1 = false;
     private void Start()
     {
         //gets mesh render for the main screen
         screenMR = shipScreenMatSwapObject.GetComponent<MeshRenderer>();
-        startingPos = whaleTerrainParentObject.transform.position;
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
     }
     //main funtion that gets called to start the whole launch sequence
     public void StartShipLaunch()
@@ -70,34 +71,79 @@ public class ShipLauncher : MonoBehaviour
         //disables door opener code
         shipDoorOpener.enabled = false;
     }
+    void ShipFlight()
+    {
+        //gets current terrain/whale parent object position
+        Vector3 objectToMovePos = whaleTerrainParentObject.transform.position;
+        //moves ship only on x until flightStep is set to 2 then moves on x & y
+        if (currentFlightStep == 1)
+        {
+            ShipFlightForward(objectToMovePos);
+        } else
+        {
+            ShipFlightForwardAndUp(objectToMovePos);
+        }
+        //calls itself and stops when flight step 3 is reached
+        if (currentFlightStep != 3)
+        {
+            Invoke("ShipFlight", 0.02f);
+        } else
+        {
+            //rolls credits when flight is done
+            RollCredits();
+        }
+    }
+    void ShipFlightForward(Vector3 objectPos)
+    {
+        //moves terrain back easing into the movement
+        whaleTerrainParentObject.transform.position = new Vector3(objectPos.x - (useMaxSpeed ? maxFlightSpeed : SpeedEaseIn(lastSpeedUsed)), objectPos.y, objectPos.z);
+        if (Vector3.Distance(transform.position, objectPos) >= 100)
+        {
+            currentFlightStep = 2;
+            lastSpeedUsed = 0.01f;
+        }
+    }
+    void ShipFlightForwardAndUp(Vector3 objectPos)
+    {
+        //moves terrain back and up easing into the vertical movement
+        whaleTerrainParentObject.transform.position = new Vector3(objectPos.x - maxFlightSpeed, objectPos.y - (useMaxSpeed1 ? maxFlightSpeed : SpeedEaseIn(lastSpeedUsed)), objectPos.z);
+        //stops movement when terrain is out of sight
+        if (Vector3.Distance(transform.position, objectPos) >= 500)
+        {
+            currentFlightStep = 3;
+        }
+    }
+    float SpeedEaseIn(float currentSpeed)
+    {
+        //starts at a slow speed and slowly ramps up to max
+        float speedToUse = 0;
+        if (currentSpeed < maxFlightSpeed)
+        {
+            speedToUse = currentSpeed * 1.01f;
+        } else if (currentFlightStep == 1)
+        {
+            useMaxSpeed = true;
+        } else if (currentFlightStep == 2)
+        {
+            useMaxSpeed1 = true;
+        }
+        lastSpeedUsed = speedToUse;
+        return speedToUse;
+    }
     bool CheckIfLaunchConditionsMet()
     {
         if (skipPlayerCountCheck)
         {
             return true;
-        } else
+        }
+        else
         {
             //place the code to check if both players are in the ship here!
             return false;
         }
-        
     }
-    void ShipFlight()
+    void RollCredits()
     {
-        if (currentFlightStep == 1)
-        {
-
-        }
-    }
-    void ShipFlightX()
-    {
-        if (Vector3.Distance(startingPos, transform.position) >= 100)
-        {
-
-        }
-    }
-    void ShipFlightY()
-    {
-        
+        //fill with whatever is needed to roll the credits/trigger another function to so do, if you don't, I will just make it do a star-wars intro style roll of the entire bee movie script. :3
     }
 }
